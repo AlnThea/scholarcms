97019864fd58121bbc1096a55fe3d8bdd44ad5c2
dahlan1991 <dahlan.fauzi1991@gmail.com>
2026-08-20 15:09:00 +0700

    Make robots.txt host dynamic

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,5 +1,14 @@
+import { headers } from 'next/headers';
+
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://scholarcms.com';
+  const headersList = headers();
+  const host = headersList.get('x-forwarded-host') || headersList.get('host') || 'scholarcms.com';
+  const protocol = headersList.get('x-forwarded-proto') || 'https';
+  let baseUrl = `${protocol}://${host}`;
+  
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+  }
 
   return {
     rules: {
